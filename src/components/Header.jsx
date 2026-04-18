be0218120888791901eac721c2d64437ce0479ed
jawasakher <jawasakher@example.com>
2026-04-18 23:31:50 +0300

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,46 @@
 import React from 'react'
 import{Link} from 'react-router-dom'
 import{assets} from '../assets/data'
+import Navbar from './Navbar'
 
 const Header = () => {
   return (
-    <header>
+    <header className=''>
 
-    <div>
+    <div className='max-padd-container'>
       {/*Container */}
-      <div>
+      <div className='flexBetween'>
         {/** Logo */}
-        <div>
+        <div className='flex-1'>
             <Link to={'/'}>
-            <img src={assets.logoImg} alt="LogoImg" className='h-11'/>
+            <img src={assets.logoImg} alt="LogoImg" className='h-20'/>
 
             </Link>
             </div>
             {/*Nabar */}
             <Navbar/>
-            {/*Button  & Profile*/}
+            {/*Buttons SearchBar  & Profile*/}
+            <div className='flex sm:flex-1 items-center sm:justify-end gap-x-4 sm:gap-x-8'>
+              {/** SearchBar */}  
             <div>
-
+            SearchBar
+            </div>
+            {/** Menu Togggle  */}
+            <>
+            MenuBtn
+            </>
+            {/** User Profile */}
+            <div>
+                {/** User */}
+                  <div>
+                    <div>
+                        <button className='btn-secondary flexCenter gap-2 rounded-full'>
+                            Login
+                            <img src={assets.user} alt="userIcon"/>
+                        </button>
+                    </div>
+                  </div>
+            </div>
             </div>
       </div>
     </div>
--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Listing = () => {
   return (
     <div>
-      
+      Listing
     </div>
   )
 }
