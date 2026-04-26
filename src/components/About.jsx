c272f69320120f1038b5abbfa9c64b3fa8749b31
jawasakher <jawasakher@example.com>
2026-04-26 12:37:01 +0300

    Optimize image loading speed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-
+import Title from '../components/Title'
 const About = () => {
   return (
-    <div>
+    <section>
+      {/** Contoiner */}
+      <div>
+        {/** Info - Left Side */}
+        <div>
+          <Title/>
+        </div>
+      </div>
       
-    </div>
+    </section>
+    
   )
 }
 
