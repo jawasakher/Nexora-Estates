2f9cf318f1c327ce21c92095c720ebdf2bfd2f94
jawasakher <jawasakher@example.com>
2026-05-01 00:01:57 +0300

     Finishing the “Helping You Find Your Dream Home” section and starting work on styling the footer in the Home landing page.

--- src/components/Cta.jsx
+++ src/components/Cta.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
-
+import {assets} from '../assets/data'
 const Cta = () => {
   return (
-    <div>
-      
+   <section class="bg-[#fffbee] pt-16 xl:pt-22">
+    <div class="max-padd-container mx-2 md:mx-auto p-px ">
+    <div class="flex flex-col items-center justify-center text-center py-12 md:py-16 rounded-[15px]">  
+        <div class="flex center bg-black/80 text-white px-3 py-1.5 ring-1 ring-slate-900/10  gap-1 rounded-full text-xs">
+            <img src={assets.rocket} alt="" width={17} className="invert" />
+            <span class="">Trusted by Experts</span>
+        </div>
+        <h2 class="h2 mt-2">
+            Sell or Rent Faster With<span className='text-seconder'> Expert Guidance</span><br /> and Proven Results! <br />
+        </h2>
+        <p class="text-slate-500 mt-2 max-w-lg max-md:text-sm">
+          Achieve your goals faster with personalized strategies, hands-on support, and results that speak for themselves.</p>
+        <button type="button" 
+        className="btn-secondary mt-4"> 
+            Get Started
+        </button>
     </div>
+    </div>
+</section>
+      
+  
   )
 }
 
--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,31 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {assets} from '../assets/data'
+import Title from '../components/Title'
+
 const Faq = () => {
+  const [openIndex, setOpenIndex] = React.useState(null)
+
+  const faqsData = [
+    {
+      question: 'Lightning-Fast Booking',
+      answer: 'Desibned for speed - instant search and seamless property viewing'
+    },
+    {
+      question: 'Fully Customizable Homes',
+      answer: 'Easily change layouts, features, and dasigns to fit your lifestyle'
+    },
+    {
+      question: 'Responsive by Location',
+      answer: 'Every property is optimized for its location — seamless browsing and viewing experience.'
+    },
+    {
+      question: 'Real Estate Powered',
+      answer: 'Backed using trusted property data - no extra agents or steps needed.'
+    },
+    {
+      question: 'Smart Home Support',
+      answer: 'All houses come ready with modern smart living features include.'
+    },
+  ];
+
+  
+
   return (
     <section className='max-padd-container py-16 xl:py-22'>
-      {/** Contatner */}
+      {/** container */}
       <div className='flex flex-col gap-y-12 xl:flex-row'>
-        {/** Image - Left Side */}
-        <div className="flex-1">
-          <div className="relative rounded-3xl overflow-hidden inline-block">
-            <img src={assets.faq}alt="faqImg" className=' block w-full '/>
+        {/** Image -Left Side  */}
+        <div className='flex-1'>
+          <div className='relative rounded-3xl overflow-hidden inline-block'>
+            <img src={assets.faq} alt='faqImg' className='block w-full'/>
             <div className='absolute top-5 left-5 right-5 bg-white p-3 rounded-2xl flex items-center gap-4 z-10'>
-              <img src={assets.signature} alt="signImg" width={55}/>
+              <img src={assets.signature} alt='signImg' width={55}/>
               <div>
-                <h5>Trusted Real Estate Experts</h5>
+                <h5 className='bold-16'>Trusted Real Estate Experts</h5>
                 <p>With over 20 years of experience, our team of professionals is dedicated to helping you find your dream property.</p>
               </div>
-              </div>
             </div>
+          </div>
         </div>
-        {/** FAQs - Right Side  */}
-        <div className="flex-1">
-          
+      {/**FAQs - Right Side */}
+        <div className='flex-1 flex flex-col justify-center'>
+          <Title
+            title1={'Homes Made for Living'}
+            title2={'Helping You Find Your Dream Home'}
+            para={'Proactively answering FAQs boosts user confidence and cuts down on support tickets.'}
+            titleStyles={'mb-10'}
+          />
+
+
+            <div className='max-w-xl w-full mt-6 flex flex-col gap-4 items-start text-left'>
+              {faqsData.map((faq, index) => (
+                
+                  <div key={index} className='flex flex-col items-startw-full' >
+                    <div className='flex items-center justify-between w-full cursor-pointer bg-secondary/10 border border-slate-900/10 p-2 px-4 rounded-lg'
+                      
+                      onClick={() => setOpenIndex( openIndex === index ? null:index)} >
+                      
+                    
+                      <h2 className='text-sm'>{faq.question}</h2>
+                       <img src={assets.down} alt=""/>   
+                   </div>
+                    <p className={`text-sm text-slate-500 px-4 transition-all duration-500 ease-in-out ${openIndex === index ? "opacity-100 max-h-[300px] translate-y-0 pt-4" : "opacity-0 max-h-0 -translate-y-2"}`}>
+                      {faq.answer}
+                    </p>
+                  </div>
+               
+              ) )}
+            </div>
+          </div>
         </div>
-      </div>
+     
     </section>
-      
-    
   )
 }
 
