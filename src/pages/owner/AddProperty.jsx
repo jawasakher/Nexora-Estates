75d3b112e37a899b84b78f9570497973740a57c0
jawasakher <jawasakher@example.com>
2026-05-15 20:40:08 +0300

    Fixing the issue where the Dashboard file contents were not displaying properly.

--- src/pages/owner/AddProperty.jsx
+++ src/pages/owner/AddProperty.jsx
@@ -1,9 +1,124 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const AddProperty = () => {
-  return (
-    <div>
+  const [images, setImages ] = useState ({
+    1:null,
+    2:null,
+    3:null,
+    4:null,
+  })
+  const [inputs, setInputs] = useState({
+    title: '',
+    description: '',
+    city: '',
+    country: '',
+    address: '',
+    area:'',
+    propertyType:'',
+    priceRent:'',
+    priceSale:'',
+    bedrooms:'',
+    garages:'',
+    amenities:{
+      parking:false,
+      Wifi:false,
+      Backyard:false,
+      Terrace:false,
       
+    },
+  })
+ 
+  const [loading, setLoading] = useState(false)
+ 
+
+  return (
+    <div className=''>
+      <form className="">
+        <div className='w-full'>
+          <h5 className='h5'>Property Name</h5>
+          <input onChange={(e)=>setInputs({...inputs, title:e.target.value})}
+          value={inputs.title}
+           type="text"
+           placeholder='Type here...'
+           className='px-3 py-1.5 ring-1 ring-slate-900/10 rounded-lg bg-secondary/5 mt-1 w-full'
+           />
+        </div>
+               
+                
+        <div className='w-full'>
+          <h5 className='h5'>Property Description</h5>
+          <textarea
+          onChange={(e)=>setInputs({...inputs, description:e.target.value})}
+          value={inputs.description}
+          rows={5}
+           type="text"
+           placeholder='Type here...'
+           className='px-3 py-1.5 ring-1 ring-slate-900/10 rounded-lg bg-secondary/5 mt-1 w-full'
+           />
+        </div>
+        <div className='flex gap-4'>
+          <div className='w-full'>
+          <h5 className='h5'>City</h5>
+          <textarea
+          onChange={(e)=>setInputs({...inputs, city:e.target.value})}
+          value={inputs.city}
+           type="text"
+           placeholder='Type here...'
+           className='px-3 py-1.5 ring-1 ring-slate-900/10 rounded-lg bg-secondary/5 mt-1 w-full'
+           />
+        </div>
+          <div className='w-full'>
+          <h5 className='h5'>Country</h5>
+          <textarea
+          onChange={(e)=>setInputs({...inputs, country:e.target.value})}
+          value={inputs.country}
+           type="text"
+           placeholder='Type here...'
+           className='px-3 py-1.5 ring-1 ring-slate-900/10 rounded-lg bg-secondary/5 mt-1 w-full'
+           />
+        </div>
+          <div className='w-full'>
+          <h5 className='h5'>Property Type</h5>
+          <select
+          onChange={(e)=>setInputs({...inputs, propertyType:e.target.value})}
+          value={inputs.propertyType}
+           className='w-36 px-3 py-2 ring-1 ring-slate-900/10 rounded-lg bg-secondary/5 mt-1'
+           >
+            <option value="">Select Property Type</option>
+             <option value="House">House</option>
+            <option value="Apartment">Apartment</option>
+            <option value="Villa">Villa</option>
+            <option value="Condo">Condo</option>
+            <option value="Townhouse">Townhouse</option>
+            <option value="Commercial">Commercial</option>
+            <option value="Land Plot">Land Plot</option>
+          </select>
+        </div>
+        </div>
+          <div className='flex gap-4 flex-wrap w-full'>
+          <div className='flex-1'>
+          <h5 className='h5'>Address</h5>
+          <textarea
+          onChange={(e)=>setInputs({...inputs, address:e.target.value})}
+          value={inputs.address}
+           type="text"
+           placeholder='Type here...'
+           className='px-3 py-1.5 ring-1 ring-slate-900/10 rounded-lg bg-secondary/5 mt-1 w-full'
+           />
+        </div>
+        <div className='w-32'>
+          <h5 className='h5'>Area</h5>
+          <textarea
+          onChange={(e)=>setInputs({...inputs,Area:e.target.value})}
+          value={inputs.Area}
+          rows={5}
+           type="number"
+           placeholder='Area (sq ft)'
+           className='px-3 py-1.5 ring-1 ring-slate-900/10 rounded-lg bg-secondary/5 mt-1 w-full'
+           />
+        </div>
+        </div>
+      </form>
     </div>
   )
 }
--- src/pages/owner/Dashboard.jsx
+++ src/pages/owner/Dashboard.jsx
@@ -1,60 +1,128 @@
 import React, { useState } from 'react'
 import { assets, dummyDashboardData } from '../../assets/data.js'
+
 const Dashboard = () => {
+
   const [dashboardData] = useState(dummyDashboardData)
 
+  const currency = import.meta.env.VITE_CURRENCY
+
   return (
-    <div className="">
-      <div>
-        <div>
-          <img src={assets.house} alt="" className='hidden sm:flex w-8'/>
+    <div className="md:px-8 py-6 xl:py-8 m-1 sm:m-3 h-[97vh] overflow-y-scroll lg:w-11/12 bg-white shadow rounded-xl">
+
+      {/* Top Cards */}
+      <div className='grid grid-cols-2 gap-4'>
+
+        <div className="flexStart gap-7 p-5 bg-[#fff4d2] lg:min-w-56 rounded-xl">
+          <img
+            src={assets.house}
+            alt="house"
+            className='hidden sm:flex w-8'
+          />
+
           <div>
-            <h4 className='h4'>{dashboardData.totalBookings.toString().padStart(2, "0")}</h4>
-            <h5 className='h5 text-secondary'>Total Sales</h5>
+            <h4 className='h4'>
+              {dashboardData.totalBookings.toString().padStart(2, "0")}
+            </h4>
+
+            <h5 className='h5 text-secondary'>
+              Total Sales
+            </h5>
           </div>
         </div>
-        <div>
-          <img src={assets.dollar} alt=""
-          className='hidden sm:flex w-8'/>
+
+        <div className="flexStart gap-7 p-5 bg-[#d1e8ff] lg:min-w-56 rounded-xl">
+
+          <img
+            src={assets.dollar}
+            alt="dollar"
+            className='hidden sm:flex w-8'
+          />
+
           <div>
-            <h4 className='h4'>{dashboardData.totalRevenue}</h4>
-            <h5 className='h5 text-secondary'>Total Revenue</h5>
+            <h4 className='h4'>
+              {currency}{dashboardData.totalRevenue}
+            </h4>
+
+            <h5 className='h5 text-secondary'>
+              Total Revenue
+            </h5>
           </div>
+        </div>
+
+      </div>
+
+      {/* Latest Bookings */}
+      <div className="mt-4">
+
+        {/* Table Header */}
+        <div className="flex justify-between flex-wrap gap-2 sm:grid sm:grid-cols-[2fr_2fr_1fr] lg:grid-cols-[0.5fr_2fr_2fr_1fr_1fr] px-6 py-3 bg-secondary border-b border-slate-900/15 rounded-t-xl">
+
+          <h5 className="h5 hidden lg:block">Index</h5>
+          <h5 className="h5">Property</h5>
+          <h5 className="h5">Booking Dates</h5>
+          <h5 className="h5">Amount</h5>
+          <h5 className="h5">Status</h5>
+
+        </div>
+
+        {/* Table Body */}
+        <div>
+
+          {dashboardData.bookings?.map((booking, index) => (
+
+            <div
+              key={index}
+              className='flex justify-between items-center flex-wrap gap-2 sm:grid sm:grid-cols-[2fr_2fr_1fr_1fr] lg:grid-cols-[0.5fr_2fr_2fr_1fr_1fr] px-6 py-3 bg-secondary/5 text-gray-700 medium-14 border-b border-slate-900/15'
+            >
+
+              <div className="hidden lg:block">
+                {index + 1}
+              </div>
+             <div className='flexStart gap-x-2 max-w-64'>
+              {/* Property */}
+              
+                 <div className='overflow-hidden rounded-lg'>
+                <img
+                  src={booking?.property?.images?.[0]}
+                  alt={booking?.property?.title || 'Property'}
+                  className="w-16 h-16 object-cover rounded-lg"
+                />
+
+                <div className='line-clamp-2'>
+                  {booking?.property?.title}
+                </div>
+
+              </div>
+
+              {/* Dates */}
+              <div>
+                {new Date(booking.checkInDate).toDateString()} 
+                to 
+                {new Date(booking.checkOutDate).toDateString()}
+              </div>
+
+              {/* Amount */}
+              <div>
+                {currency}{booking.totalPrice}
+              </div>
+
+              {/* Status */}
+              
+                <button className={`${booking.isPaid ? "bg-green-400/80 text-white" : "bg-secondary/10 text-red-500"}w-22 py-0.5 rounded-full text-xs border-green-500/30`}>
+                  {booking.status}
+                </button>
+              </div>
+
+            </div>
+
+          ))}
+
         </div>
       </div>
-      {/** latest Booking/sates */}
-      <div>
-        <div>
-          <h5 className="h5">Index</h5>
-           <h5 className="h5">Property</h5>
-            <h5 className="h5">Booking dates</h5>
-             <h5 className="h5">Amount</h5>
-              <h5 className="h5">Status</h5>
-          </div>
-          <div>
-            {dashboardData.bookings?.map((booking, index) => (
-              <div key={index} className='flex items-center gap-4'>
-                <div>{index + 1}</div>
-                <div>
-                  <div>
-                    <img
-                      src={booking?.property?.images?.[0]}
-                      alt={booking?.property?.title || 'Property'}
-                      className="w-16 rounded-lg"
-                    />
-                  </div>
-                  <div className='line-clamp-2'>{booking?.property?.title}</div>
-                  <div>
-                    {new Date(booking.checkInDate).toDateString()} to {new Date(booking.checkOutDate).toDateString()}
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
 
     </div>
-  );
-};
+  )
+}
 
-export default Dashboard;
+export default Dashboard
